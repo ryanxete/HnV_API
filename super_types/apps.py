ed82e1ca8317821d5ae85a26cfc4ece3115a57b6
Ryan Etedali <ryanetedali@gmail.com>
2022-07-15 17:26:23 -0500

    regirstered apps and restframework to installed apps + created supertypes model

--- super_types/apps.py
+++ super_types/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SuperTypeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'super_type'
+    name = 'super_types'
--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class SuperType(models.Model):
+    hero = models.CharField(max_length=100)
+    villain = models.CharField(max_length=100)
