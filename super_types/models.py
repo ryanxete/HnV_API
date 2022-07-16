d64d6bdab523bc58a4f29a87daae8bffc0fac39f
Ryan Etedali <ryanetedali@gmail.com>
2022-07-16 15:50:00 -0500

    created supertypes' url.py + created first views function

--- super_types/models.py
+++ super_types/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 # Create your models here.
 class SuperType(models.Model):
-    hero = models.CharField(max_length=100)
-    villain = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
---
+++ super_types/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('super_types/', views.yea)
+]
