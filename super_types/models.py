9912e4206d77bd22fdc331bf3a672cfa24fe0099
Ryan Etedali <ryanetedali@gmail.com>
2022-07-18 17:50:50 -0500

    created FK relation

--- super_types/models.py
+++ super_types/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from supers.models import Super
 
 # Create your models here.
 class SuperType(models.Model):
     type = models.CharField(max_length=100)
-    super = models.ForeignKey(Super, on_delete=models.CASCADE, null=True)
--- supers/models.py
+++ supers/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from super_types.models import SuperType
 
-# Create your models here.
 class Super(models.Model):
     name = models.CharField(max_length=300)
     alter_ego = models.CharField(max_length=300)
     primary_ability = models.CharField(max_length=300)
     secondry_primary = models.CharField(max_length=300)
     catchphrase = models.CharField(max_length=300)
-    # super_type = ForeignKey
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE, null=True)
